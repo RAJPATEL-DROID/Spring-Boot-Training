48116b140f04f680603d71b501317e5aceeb3176
RAJ PATEL <raj1729patel@gmail.com>
2024-06-26 19:05:42 +0530

    NEW:API Doc, i18n, API Versioning

--- Spring/restwithspring/src/main/java/com/restapi/restwithspring/helloworld/HelloWorlRest.java
+++ Spring/restwithspring/src/main/java/com/restapi/restwithspring/helloworld/HelloWorlRest.java
@@ -1,21 +1,43 @@
 package com.restapi.restwithspring.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContext;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorlRest {
 
-    @RequestMapping("/hello-world")
+    private MessageSource messageSource;
+
+    public HelloWorlRest(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @RequestMapping(path = "/hello-world", method = RequestMethod.GET)
     public String helloWorld() {
         return "Hello World";
     }
 
-    @RequestMapping("/hello-world-bean/{name}")
+    @RequestMapping(value = "/hello-world-bean/{name}", method = RequestMethod.GET)
     public HelloWorldBean helloWorldBean(@PathVariable String name) {
-
         return new HelloWorldBean("How Are you " + name + " ?");
+
+    }
+
+
+    @RequestMapping(path = "/hello-world-internationalised", method = RequestMethod.GET)
+    public String helloWorldInternationalised() {
+
+        Locale locale = LocaleContextHolder.getLocale();
+
+        return messageSource.getMessage("good.morning.message",null,"Default Message",locale);
+
 
     }
 
--- Spring/restwithspring/src/main/java/com/restapi/restwithspring/survey/SurveyResources.java
+++ Spring/restwithspring/src/main/java/com/restapi/restwithspring/survey/SurveyResources.java
@@ -1,5 +1,7 @@
 package com.restapi.restwithspring.survey;
 
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +10,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class SurveyResources {
@@ -19,21 +23,27 @@ public class SurveyResources {
 
     }
 
-    @RequestMapping("/surveys")
+    @RequestMapping(path = "/surveys", method = RequestMethod.GET)
     public List<Survey> getAllSurveys() {
         return surveyService.retrieveAllSurveys();
     }
 
-    @RequestMapping("/surveys/{id}")
-    public Survey getSurveyById(@PathVariable String id) {
+    @RequestMapping(path = "/surveys/{id}" , method = RequestMethod.GET)
+    public EntityModel<Survey> getSurveyById(@PathVariable String id) {
         Survey survey = surveyService.retrieveSurveyById(id);
         if(survey == null) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
-        return survey;
+
+        EntityModel<Survey> entityModel = EntityModel.of(survey);
+
+        WebMvcLinkBuilder link = WebMvcLinkBuilder.linkTo(methodOn(this.getClass()).getAllSurveys());
+
+        entityModel.add(link.withRel("all-surveys"));
+        return entityModel;
     }
 
-    @RequestMapping("/surveys/{id}/questions")
+    @RequestMapping(path ="/surveys/{id}/questions", method = RequestMethod.GET)
     public List<Question> retrieveAllSurveyQuestions(@PathVariable String id) {
 
         List<Question> questions = surveyService.retrieveAllSurveyQuestions(id);
@@ -45,7 +55,7 @@ public class SurveyResources {
         return questions;
     }
 
-    @RequestMapping("/surveys/{id}/questions/{questionId}")
+    @RequestMapping(path = "/surveys/{id}/questions/{questionId}", method = RequestMethod.GET)
     public Question retrieveSpecificSurveyQuestion(@PathVariable String id, @PathVariable String questionId) {
 
         Question question = surveyService.retrieveSpecificSurveyQuestion(id,questionId);

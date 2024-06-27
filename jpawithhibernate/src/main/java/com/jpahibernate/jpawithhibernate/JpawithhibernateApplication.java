529228a4c59aaa975d5815840d885dd02ca76f26
RAJ PATEL <raj1729patel@gmail.com>
2024-06-27 10:39:02 +0530

    ADD:JPAProject

---
+++ jpawithhibernate/src/main/java/com/jpahibernate/jpawithhibernate/JpawithhibernateApplication.java
@@ -0,0 +1,13 @@
+package com.jpahibernate.jpawithhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpawithhibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpawithhibernateApplication.class, args);
+	}
+
+}
---
+++ jpawithhibernate/src/test/java/com/jpahibernate/jpawithhibernate/JpawithhibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpahibernate.jpawithhibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpawithhibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
